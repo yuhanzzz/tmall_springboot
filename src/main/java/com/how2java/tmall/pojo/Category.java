088953ee4afd2b944ba18ee3471de2996bca32f3
yuhanzzz <631500250@qq.com>
2018-11-21 18:03:24 +0800

    增加前台首页

--- src/main/java/com/how2java/tmall/pojo/Category.java
+++ src/main/java/com/how2java/tmall/pojo/Category.java
@@ -1,13 +1,10 @@
 package com.how2java.tmall.pojo;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
 
 @Entity
 @Table(name = "category")
@@ -20,6 +17,27 @@ public class Category {
     int id;
 
     String name;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<List<Product>> getProductsByRow() {
+        return productsByRow;
+    }
+
+    public void setProductsByRow(List<List<Product>> productsByRow) {
+        this.productsByRow = productsByRow;
+    }
+
+    @Transient
+    List<Product> products;
+    @Transient
+    List<List<Product>> productsByRow;
 
     public int getId() {
         return id;
